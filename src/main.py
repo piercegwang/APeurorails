7f83ce943d39b7b62342d766ff1673f02253a59a
Austin Wang <austinw@mit.edu>
2017-12-27 00:10:24 -0800

    Version 2.0 finished: added visual output, path finding

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from optparse import OptionParser
 DEFAULT_DB_PATH = "../database/eurorails.json"
 DEFAULT_BOARD_PATH = "../database/board_ascii.txt"
 DEFAULT_IMG_PATH = "../database/board.jpg"
+DEFAULT_HARBOR_PATH = "../database/harbors.txt"
 
 def print_city(database, city):
     entry = database["cities"][city]
@@ -21,47 +22,101 @@ def print_city(database, city):
 
     return output
 
-def handle_query(database, visual, query):
+def handle_query(database, board, visual, query):
+    tokenized = query.split(' ')
+    output = None
+    
     # QUERY = clear board
-    if query == "clear":
-        visual.clear()
-        return "string string string string board cleared string string"
+    if tokenized[0] == "clean":
+        visual.clean()
 
     # QUERY = city
-    elif query in database["cities"]:
-        visual.mark_city(database["cities"][query]["coords"], 0)
-        return print_city(database, query)
+    elif len(tokenized) == 1 and tokenized[0] in database["cities"]:
+        output = ""
+        visual.mark_city(database["cities"][tokenized[0]]["coords"], 0)
+        output += print_city(database, tokenized[0])
+        output += "\n    "
 
     # QUERY = load
-    elif query in database["loads"]:
-        entry = database["loads"][query]
+    elif len(tokenized) == 1 and tokenized[0] in database["loads"]:
+        entry = database["loads"][tokenized[0]]
         output = ""
         for city in entry:
+            visual.mark_city(database["cities"][city]["coords"], 0)
             output += print_city(database, city) + "\n    "
-        return output
 
-    else:
+    # QUERY = two cities
+    elif len(tokenized) == 2 and tokenized[0] in database["cities"] and tokenized[1] in database["cities"]:
+        start_city = tokenized[0]
+        end_city = tokenized[1]
+        output = ""
+
+        visual.mark_city(database["cities"][start_city]["coords"], 0)
+        visual.mark_city(database["cities"][end_city]["coords"], 0)
+        output += print_city(database, start_city) + "\n    "
+        output += print_city(database, end_city) + "\n    "
+        
+        city_loc_1 = database["cities"][start_city]["coords"]
+        city_loc_2 = database["cities"][end_city]["coords"]
+        path, cost = find_path(city_loc_1, city_loc_2, board)
+        output += "Optimal Cost Path: cost " + str(cost) + ", length" + str(len(path))
+        # for pt in path:
+        #     output += str(pt) + " "
+        output += "\n    "
+        visual.draw_path(path, 0)
+
+    # QUERY = destination city and load
+    elif len(tokenized) == 2 and tokenized[0] in database["cities"] and tokenized[1] in database["loads"]:
+        end_city = tokenized[0]
+        start_cities = database["loads"][tokenized[1]]
+        output = ""
+
+        for sc in start_cities:
+            visual.mark_city(database["cities"][sc]["coords"], 0)
+            output += print_city(database, sc) + "\n    "
+        visual.mark_city(database["cities"][end_city]["coords"], "red")
+        output += print_city(database, end_city) + "\n    "
+
+        i = 1
+        for sc in start_cities:
+            city_loc_1 = database["cities"][sc]["coords"]
+            city_loc_2 = database["cities"][end_city]["coords"]
+            path, cost = find_path(city_loc_1, city_loc_2, board)
+            output += "Optimal Cost Path " + str(i) + ": " + sc + ", cost " + str(cost) + ", length " + str(len(path))
+            # for pt in path:
+            #     output += str(pt) + " "
+            output += "\n    "
+            i += 1
+            visual.draw_path(path, 0)
+        
+    elif len(query.strip()) > 0:
         raise ValueError("Query could not be processed.")
+
+    return output
+    
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("--database", dest="db_path", default=str(DEFAULT_DB_PATH))
     parser.add_option("--board", dest="board_path", default=str(DEFAULT_BOARD_PATH))
+    parser.add_option("--harbors", dest="harbor_path", default=str(DEFAULT_HARBOR_PATH))
     opts, args = parser.parse_args()
 
-    database, board = load_files(opts.db_path, opts.board_path)
+    database, board = load_files(opts.db_path, opts.board_path, opts.harbor_path)
     visual = Visual(DEFAULT_IMG_PATH)
 
     i = 1
     while True:
         visual.update()
         raw_expression = input(str(i) + ": ")
-        if raw_expression == "exit":
+        if raw_expression == "reload":
+            database, board = load_files(opts.db_path, opts.board_path, opts.harbor_path)
+        elif raw_expression == "exit":
             visual.quit()
             break
         else:
             try:
-                output = handle_query(database, visual, raw_expression)
+                output = handle_query(database, board, visual, raw_expression)
                 if output is not None:
                     print("  >", output)
             except Exception as error:
--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,13 @@ import tkinter as tk
 import json
 
 class Board:
-    def __init__(self, board, min_x, max_x, min_y, max_y):
+    def __init__(self, board, harbors, min_x, max_x, min_y, max_y):
         self.min_x = min_x
         self.max_x = max_x
         self.min_y = min_y
         self.max_y = max_y
         self.board = board
+        self.harbors = harbors
         
     def __call__(self, x, y):
         x_index = 2 * (x - self.min_x)
@@ -24,14 +25,19 @@ class Board:
     def is_harbor(self, x, y):
         return self(x, y) == "h" or self(x, y) == "H"
     
+    def get_other_harbor(self, x, y):
+        if self.is_harbor(x, y):
+            harbor, _ = self.harbors[(x, y)]
+            return harbor
+    
     def compute_cost(self, p1, p2):
         def get_travel_cost(x, y, dx, dy):
             x_index = 2 * (x - self.min_x) + dx
-            y_index = 2 * (self.max_y - y) + dy
+            y_index = 2 * (self.max_y - y) - dy
             if 0 <= x_index < len(self.board[0]) and 0 <= y_index < len(self.board):
                 cost = self.board[y_index][x_index]
             else:
-                raise ValueError("Point out of range.")
+                raise ValueError("Point " + str((x, y, dx, dy)) + " out of range.")
             if cost == ' ':
                 return 0
             else:
@@ -58,9 +64,12 @@ class Board:
             elif dest == "S" or dest == "M":
                 cost = 3
             elif dest == "h":
-                raise NotImplementedError("Harbors not implemented.")
+                other_harbor, cost = self.harbors[(p2_x, p2_y)]
+                if self(other_harbor[0], other_harbor[1]) == "H":
+                    cost += 3
             elif dest == "H":
-                raise NotImplementedError("Harbors not implemented.")
+                _, cost = self.harbors[(p2_x, p2_y)]
+                cost += 3
             else:
                 raise ValueError("Board contains invalid character.")
             
@@ -68,10 +77,33 @@ class Board:
             return cost
 
 class Visual:
-    DEFAULT_BOARD_IMG_PATH = "../database/board.jpg"
-    LINE_THICKNESS = 10
-    CIRCLE_THICKNESS = 100
-    RADIUS = 64
+    LINE_THICKNESS = 2
+    CIRCLE_THICKNESS = 2
+    RADIUS = 6
+    references = {(0, 0): (505, 402), # Paris
+                  (-9, -19): (210, 669), # Madrid
+                  (15, -13): (645, 583), # Milano
+                  (26, -8): (860, 513), # Wien
+                  (17, 5): (820, 330), # Berlin
+                  (10, 20): (827, 117), # Goteburg
+                  (-10, 16): (468, 177), # Manchester
+                  (7, 4): (650, 344), # Ruhr
+                  (-21, -17): (32, 637), # Lisboa
+                  (-14, -25): (81, 749), # Sevilla
+                  (1, -14): (410, 598), # Toulouse
+                  (21, -15): (722, 611), # Venezia
+                  (25, -25): (705, 749), # Roma
+                  (39, -19): (983, 665), # Beograd
+                  (35, -22): (895, 708), # Sarajevo
+                  (28, -15): (836, 611), # Zagreb
+                  (-21, 17): (304, 163), # Cork
+                  (27, 2): (958, 373), # Lodz
+                  (8, 9): (705, 274), # Bremen
+                  (-18, 19): (363, 135), # Dublin
+                  (-17, 22): (402, 93), # Belfast
+                  }
+    X_CONST = (16.21, -0.1)
+    Y_CONST = (8.0645, 14.03)
 
     def __init__(self, board_file):
         self.filepath = board_file
@@ -85,15 +117,16 @@ class Visual:
         self.panel = tk.Label(self.window, image=self.tk_img)
         self.panel.pack(side="bottom", fill="both", expand="yes")
 
-    def draw_path(self, p1, p2, color="black"):
-        p1 = self.CtoP(p1)
-        p2 = self.CtoP(p2)
-        self.draw.line([p1, p2], fill=color, width=Visual.LINE_THICKNESS)
-        print(self.board.size)
+    def draw_path(self, points, color="black"):
+        for i in range(len(points) - 1):
+            p1 = points[i]
+            p2 = points[i + 1]
+            p1 = self.coordinates_to_pixels(p1)
+            p2 = self.coordinates_to_pixels(p2)
+            self.draw.line([p1, p2], fill=color, width=Visual.LINE_THICKNESS)
 
     def mark_city(self, city_loc, color=0):
-        city_loc = self.CtoP(city_loc)
-        print(city_loc)
+        city_loc = self.coordinates_to_pixels(city_loc)
         for i in range(Visual.RADIUS, Visual.RADIUS + Visual.CIRCLE_THICKNESS):
             self.draw.ellipse((city_loc[0] - i, city_loc[1] - i, city_loc[0] + (i - 1), city_loc[1] + (i - 1)),
                               outline=color)
@@ -101,8 +134,6 @@ class Visual:
                               outline = color)
             self.draw.ellipse((city_loc[0] - i, city_loc[1] - i, city_loc[0] + i, city_loc[1] + i),
                               outline=color)
-
-
 
     def clean(self):
         self.board = Image.open(self.filepath)
@@ -116,24 +147,28 @@ class Visual:
     def quit(self):
         self.window.quit()
         
-    def CtoP(self, coords):  #Coords to pixels
-        origin = [505, 402]
-        xshift = 16.21
-        yshift = [8.11, 14.03]
-        pixel = origin
-        if coords[0] == 0: #x shifts
-            pass
-        elif coords[0] != 0:
-            pixel[0] += int(xshift * coords[0])
-
-        if coords[1] == 0: #y shifts
-            pass
-        elif coords[1] != 0:
-            pixel[0] += int(yshift[0] * coords[1])
-            pixel[1] -= int(yshift[1] * coords[1])
+    def coordinates_to_pixels(self, coords):
+        def get_closest_reference(coords):
+            def distance(p1, p2):
+                return abs(p2[1] - p1[1]) + abs(p2[0] - p1[0])
+            closest_reference = None
+            for r in Visual.references:
+                if closest_reference is None or distance(coords, r) < distance(coords, closest_reference):
+                    closest_reference = r
+            return closest_reference
+        
+        
+        ref = get_closest_reference(coords)
+        pixel = list(Visual.references[ref])
+        
+        delta = (coords[0] - ref[0], coords[1] - ref[1])
+            
+        pixel[0] += round(Visual.X_CONST[0] * (delta[0] + 0.5 * delta[1])) + round(Visual.X_CONST[1] * delta[1])
+        pixel[1] -= round(Visual.Y_CONST[1] * delta[1])
+        print(ref, pixel)
         return tuple(pixel)
 
-def load_files(city_db_path, board_path):
+def load_files(city_db_path, board_path, harbor_path):
     """
     Loads JSON database of Eurorails and ASCII map of board.
 
@@ -142,19 +177,26 @@ def load_files(city_db_path, board_path):
     :return: database as a dictionary
     """
     board = []
+    harbors = {}
     with open(city_db_path, 'r') as f:
         city_db = json.load(f)
 
-    raw = open(board_path).read().split('\n')
-    min_x, max_x = map(int, raw[0].split(' '))
-    min_y, max_y = map(int, raw[1].split(' '))
+    board_raw = open(board_path).read().split('\n')
+    min_x, max_x = map(int, board_raw[0].split(' '))
+    min_y, max_y = map(int, board_raw[1].split(' '))
 
-    for row in raw[2:]:
+    for row in board_raw[2:]:
         board.append([c for c in row])
         if len(board[-1]) < 2 * (min_x + max_x + 1):
             board[-1].extend([' ' for _ in range(abs(min_x) + abs(max_x) + 1 - len(board[-1]))])
 
-    board = Board(board, min_x, max_x, min_y, max_y)
+    harbors_raw = open(harbor_path).read().split('\n')
+    for line in harbors_raw:
+        expand = line.split(' ')
+        harbors[(int(expand[1]), int(expand[2]))] = ((int(expand[4]), int(expand[5])), int(expand[6]))
+        harbors[(int(expand[4]), int(expand[5]))] = ((int(expand[1]), int(expand[2])), int(expand[6]))
+
+    board = Board(board, harbors, min_x, max_x, min_y, max_y)
 
     return city_db, board
 
@@ -171,11 +213,16 @@ def find_path(p1, p2, board):
         neighbors = []
         for dx, dy in ((1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1), (1, -1)):
             if board.is_valid(point.x + dx, point.y + dy):
-                neighbors.append(Point((point.x + dx, point.y + dy),
-                                       point,
-                                       point.cost + board.compute_cost(point.coord, (point.x + dx, point.y + dy))))
-                if board.is_harbor(point.x + dx, point.y + dy):
-                    raise NotImplementedError("Harbors not implemented for neighbors.")
+                if not board.is_harbor(point.x, point.y) or \
+                   board.is_harbor(point.x, point.y) and not board.is_harbor(point.x + dx, point.y + dy):
+                    neighbors.append(Point((point.x + dx, point.y + dy),
+                                            point,
+                                            point.cost + board.compute_cost(point.coord, (point.x + dx, point.y + dy))))
+        if board.is_harbor(point.x, point.y):
+            neighbors.append(Point(board.get_other_harbor(point.x, point.y),
+                                   point,
+                                   point.cost))
+                
         return neighbors
     
     class LinkedList:
@@ -278,21 +325,24 @@ def find_path(p1, p2, board):
         current_point, agenda = agenda.pop_min(value=Point.get_cost)
         if current_point not in visited:
             visited.add(current_point)
-            print("Examining", current_point)
             if current_point == p2:
-                return get_path(p1, current_point)
+                return get_path(p1, current_point), current_point.get_cost()
             else:
                 neighbors = get_neighbors(current_point)
                 for n in neighbors:
                     if n not in visited:
                         agenda.append(n)
     
-    return None
+    return None, None
+    # TODO: implement A* search
+    # TODO: balance between cost and distance
 
 if __name__ == '__main__':
     DEFAULT_DB_PATH = "../database/eurorails.json"
     DEFAULT_BOARD_PATH = "../database/board_ascii.txt"
-    db, board = load_files(DEFAULT_DB_PATH, DEFAULT_BOARD_PATH)
-    path = find_path((15, -13), (18, -7), board)
+    DEFAULT_HARBOR_PATH = "../database/harbors.txt"
+    
+    db, board = load_files(DEFAULT_DB_PATH, DEFAULT_BOARD_PATH, DEFAULT_HARBOR_PATH)
+    path = find_path((-21, 17), (12, -13), board)
     # path = find_path((0, 0), (1, 1), board)
     print(path)
